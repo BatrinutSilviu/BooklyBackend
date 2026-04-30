00f02a98b85afe1e86f8e5cef16217996a76a85f
silviu <batrinutsilviu@gmail.com>
2026-04-30 16:18:17 +0300

    rename app

--- scripts/generate-swagger.ts
+++ scripts/generate-swagger.ts
@@ -17,7 +17,7 @@ const spec = createSwaggerSpec({
                 description: 'Development server',
             },
             {
-                url: 'https://book-teller-batrinutsilvius-projects.vercel.app',
+                url: 'https://bookly-backend-batrinutsilvius-projects.vercel.app',
                 description: 'Production server',
             },
         ],
