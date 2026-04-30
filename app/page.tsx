5290e313e642068ddb55e7ca20fabc675d546d75
silviu <batrinutsilviu@gmail.com>
2026-04-30 15:49:32 +0300

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
 
   return (
       <main className="p-8">
-        <h1 className="text-2xl font-bold mb-4">Home page</h1>
+        <h1 className="text-2xl font-bold mb-4">Home test page</h1>
       </main>
   )
 }
